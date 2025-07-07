2202d517cb0f6ee01abd6fb97d7de58747677312
Apurba-Acharya <apurba1907@gmail.com>
2025-07-07 07:59:26 +0530

    07/07

--- src/test/java/ex05_XPath/P4_XPathAxes.java
+++ src/test/java/ex05_XPath/P4_XPathAxes.java
@@ -1,14 +1,16 @@
 package ex05_XPath;
 
+import javafx.scene.layout.Priority;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class P4_XPathAxes {
-    @Test
-    public void XPathAxes(){
-        WebDriver driver = new ChromeDriver();
+    WebDriver driver = new ChromeDriver();
+
+    @Test(priority = 1)
+    public void XPathAxes1(){
         driver.get("https://katalon-demo-cura.herokuapp.com/profile.php#login");
 
         driver.findElement(By.xpath("//div[@class=\"form-group\"][3]//parent::form")); // Parent
@@ -41,5 +43,37 @@ public class P4_XPathAxes {
         //tagname[contains(@attribute,"value")]
         //*[contains(@attribute,"value")]
         //tagname[starts-with(@attribute,"value")]
+    }
+
+    @Test (priority = 2)
+    public void XPathAxes2(){
+        driver.get("https://www.hyrtutorials.com/p/add-padding-to-containers.html");
+
+        // Following-sibling:
+        driver.findElement(By.xpath("//label[text()=\"Email\"]/following-sibling::input[1]"));
+
+        // Parent:
+        driver.findElement(By.xpath("//label[text()=\"Email\"]/parent::div"));
+
+        // Child:
+        driver.findElement(By.xpath("//div[@class=\"container\"]/child::input[@type=\"text\"]"));
+
+        // Preceding-sibling:
+        driver.findElement(By.xpath("//td[text() = \"Maria Anders\"]/preceding-sibling::td/child::input"));
+
+        // Descendant:
+        driver.findElement(By.xpath("//div[@class=\"container\"]/descendant::button"));
+
+        // Ancestor
+        driver.findElement(By.xpath("//div[@class=\"buttons\"]/ancestor::div"));
+
+        // Ancestor-or-self: It will include current element also.
+        driver.findElement(By.xpath("//div[@class=\"buttons\"]/ancestor-or-self::div"));
+
+        //Following:
+        driver.findElement(By.xpath("//label[text()=\"Password\"]/following::input[2]"));
+
+        //Preceding:
+        driver.findElement(By.xpath("//label[text()=\"Password\"]/preceding::input[2]"));
     }
 }
