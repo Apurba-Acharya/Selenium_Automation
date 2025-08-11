bb16f9e2528eed06905770842f9f20d29fe7fd54
Apurba-Acharya <apurba1907@gmail.com>
2025-08-11 07:55:13 +0530

    11/08

--- src/test/java/ex05_XPath/P5_amazonXPathAxes.java
+++ src/test/java/ex05_XPath/P5_amazonXPathAxes.java
@@ -1,20 +1,75 @@
 package ex05_XPath;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.List;
 
 public class P5_amazonXPathAxes {
     WebDriver driver;
 
     @Test
-    public void amazonXPath(){
-        driver = new ChromeDriver();
+    public void amazonXPath() throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
+        WebDriver driver = new ChromeDriver(options);
+
         driver.get("https://www.amazon.in/");
-        driver.findElement(By.xpath("//label[text()=\"Search Amazon.in\"]/following-sibling::input")).sendKeys("Phones"); //following-sibling
-        driver.findElement(By.xpath("//span[@aria-label=\"Go\"]/child::input")).click(); //child
-        driver.findElement(By.xpath("//*[contains(@aria-label, \"Apply the filter Samsung\")]/descendant::i")).click(); // descendant
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        try {
+
+            WebElement continueBtn = wait.until(ExpectedConditions.presenceOfElementLocated(
+                    By.xpath("//*[contains(@class, 'a-button-inner')]//button")
+            ));
+
+            if (continueBtn.getText().equalsIgnoreCase("Continue shopping")) {
+                continueBtn.click();
+            }
+        } catch (TimeoutException e) {
+            System.out.println("'Continue shopping' button not found, proceeding to search...");
+        }
+
+        try {
+            WebElement searchBox = wait.until(ExpectedConditions.elementToBeClickable(
+                    By.xpath("//label[text()='Search Amazon.in']/following-sibling::input")
+            ));
+            searchBox.sendKeys("Phones");
+            driver.findElement(By.xpath("//span[@aria-label='Go']/child::input")).click();
+            wait.until(ExpectedConditions.elementToBeClickable(
+                    By.xpath("//*[contains(@aria-label, 'See more, Brands')]")
+            )).click();
+            List<WebElement> mobiles = wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(
+                    By.xpath("//*[contains(@id, 'brandsRefinements')]//*[contains(@role, 'presentation')]")
+            ));
+
+            boolean brandFound = false;
+            for (WebElement listMob : mobiles) {
+                if (listMob.getText().equalsIgnoreCase("CMF BY NOTHING")) {
+                    listMob.click();
+                    brandFound = true;
+                    break;
+                }
+            }
+
+            if (!brandFound) {
+                System.out.println("Brand 'CMF BY NOTHING' not found, continuing...");
+            }
+
+        } catch (TimeoutException e) {
+            System.out.println("Search or brand selection elements not found, skipping...");
+        }
 
     }
 }
