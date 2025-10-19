50641ba6038c86585f02380bb41c08eb8c34b5af
Apurba <apurba1907@gmail.com>
2025-10-19 08:22:21 +0530

    18/10

---
+++ src/test/java/ex28_testNG/TNG9_Grouping/TNG9_groupingTest2.java
@@ -0,0 +1,25 @@
+package ex28_testNG.TNG9_Grouping;
+
+import org.testng.annotations.Test;
+
+public class TNG9_groupingTest2 {
+    @Test(groups = {"functional", "regression", "sanity"})
+    public void test5(){
+        System.out.println("Test5");
+    }
+
+    @Test(groups = {"sanity", "smoke"})
+    public void test6(){
+        System.out.println("Test6");
+    }
+
+    @Test(groups = {"regression"})
+    public void test7(){
+        System.out.println("Test7");
+    }
+
+    @Test(groups = {"functional", "smoke"})
+    public void test8(){
+        System.out.println("Test8");
+    }
+}
