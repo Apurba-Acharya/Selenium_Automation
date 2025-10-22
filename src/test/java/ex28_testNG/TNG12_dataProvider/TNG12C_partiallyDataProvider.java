b65c44a3ba8b10d81767a9ba9f3d9ffd94876042
Apurba-Acharya <apurba1907@gmail.com>
2025-10-22 07:48:07 +0530

    17/10

--- src/test/java/ex28_testNG/TNG12_dataProvider/TNG12C_partiallyDataProvider.java
+++ src/test/java/ex28_testNG/TNG12_dataProvider/TNG12C_partiallyDataProvider.java
@@ -6,13 +6,14 @@ import org.testng.annotations.Test;
 public class TNG12C_partiallyDataProvider {
     // indices?: Whatever the data you want to pass those data indexes is called as indices
     // why use?: to pass the partial data to the test methos
-    // how to pass?:
+    // how to pass?: indices is a parameter of data provider --> @DataProvider(indices = {0,2})
+    // id we not use indices then all the value will be pass through test method.
 
     @Test(dataProvider = "dataProv1")
     public void test(String str) {
         System.out.println(str);
     }
-    @DataProvider()
+    @DataProvider(indices = {0,2})
     public String[] dataProv1() {
         String[] data = new String[]{
                 "adcd", "xyz", "pqr", "mno"
