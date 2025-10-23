11f5175ac16739e82c97da903e6886466a04aa6b
Apurba-Acharya <apurba1907@gmail.com>
2025-10-23 22:07:34 +0530

    17/10

--- src/test/java/ex28_testNG/TNG13_integrationOfExcelFileWithDataProvider/TNG13A_excelDataSupllier.java
+++ src/test/java/ex28_testNG/TNG13_integrationOfExcelFileWithDataProvider/TNG13A_excelDataSupllier.java
@@ -1,19 +1,22 @@
 package ex28_testNG.TNG13_integrationOfExcelFileWithDataProvider;
 
+import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.DataProvider;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
-public class TNG13_excelDataSupllier {
+
+public class TNG13A_excelDataSupllier {
 
     // Excel is a workBook which contains : Sheet -> Row -> Cell
     // WorkBook is 2 types: .xls->HSSF ; .xlsx->XSSF
 
-    public static void main(String[] args) throws IOException {
+    @DataProvider
+    public String [][] getData() throws IOException {
 
         //WorkBook configuration:
         File excelFile = new File("C:\\Users\\apurb\\IdeaProjects\\Selenium_Automation\\src\\test\\resources\\excelDataTNG.xlsx");
@@ -25,16 +28,22 @@ public class TNG13_excelDataSupllier {
         XSSFSheet sheet = workBook.getSheet("Sheet1");
         System.out.println(sheet.getPhysicalNumberOfRows());
         System.out.println(sheet.getLastRowNum()); // Excluding header row
-        int noOfRows = sheet.getLastRowNum();
+        int noOfRows = sheet.getPhysicalNumberOfRows();
         int noOfColumns = sheet.getRow(0).getLastCellNum();
 
-        for(int i=1; i<=noOfRows; i++){
+        String [][] data = new String [noOfRows-1][noOfColumns];
+        for(int i=0; i<noOfRows-1; i++){
             for (int j=0; j<noOfColumns; j++){
-                sheet.getRow()
+                DataFormatter df = new DataFormatter();
+                data[i][j] = (df.formatCellValue(sheet.getRow(i+1).getCell(j)));;
             }
         }
-
         workBook.close();
         fls.close();
+
+//        for(String [] dataArr : data){
+//            System.out.println(Arrays.toString(dataArr));
+//        }
+        return data;
     }
 }
