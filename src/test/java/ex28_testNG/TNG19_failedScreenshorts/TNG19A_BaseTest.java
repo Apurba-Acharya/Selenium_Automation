70b6d8cd2425e5ff6b93e767ce25f945b153b5fd
Apurba-Acharya <apurba1907@gmail.com>
2025-10-27 08:01:02 +0530

    27/10

--- src/test/java/ex28_testNG/TNG19_failedScreenshorts/TNG19A_BaseTest.java
+++ src/test/java/ex28_testNG/TNG19_failedScreenshorts/TNG19A_BaseTest.java
@@ -1,13 +1,23 @@
 package ex28_testNG.TNG19_failedScreenshorts;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class TNG19A_BaseTest {
     public static WebDriver driver;
+    public static String screenshotsSubFolderName;
     @BeforeTest
     public void setUp(){
         WebDriverManager.chromedriver().setup();
@@ -18,5 +28,23 @@ public class TNG19A_BaseTest {
     @AfterTest
     public void tearDown(){
         driver.quit();
+    }
+
+    public void captureScreenshot(String fileName) {
+        if(screenshotsSubFolderName == null) {
+            LocalDateTime myDateObj = LocalDateTime.now();
+            DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("ddMMyyyyHHmmss");
+            screenshotsSubFolderName = myDateObj.format(myFormatObj);
+        }
+
+        TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
+        File sourceFile = takesScreenshot.getScreenshotAs(OutputType.FILE);
+        File destFile = new File("./Screenshots/"+ screenshotsSubFolderName+"/"+fileName); // ./ => this is indicating the current project directory
+        try {
+            FileUtils.copyFile(sourceFile, destFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Screenshot saved successfully");
     }
 }
